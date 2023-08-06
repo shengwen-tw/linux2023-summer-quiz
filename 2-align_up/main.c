d0c705d6e1004f5041ea01dc9163852598f642ea
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-08-06 14:43:32 +0800

    Add my own solution to problem 2

---
+++ 2-align_up/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#define PRINT_ALIGN_UP(addr, n) \
+     printf("align_up(%d, %d) = %ld\n", addr, n, align_up(addr, n))
+
+static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
+{
+    uintptr_t mask = alignment - 1;
+    if ((alignment & mask) == 0) {  /* power of two? */
+        return (sz & ~mask) + ((sz & mask) > 0) * alignment;
+    }
+    return (((sz + mask) / alignment) * alignment);
+}
+
+int main(void)
+{
+    PRINT_ALIGN_UP(120, 4);
+
+    PRINT_ALIGN_UP(121, 4);
+    PRINT_ALIGN_UP(122, 4);
+    PRINT_ALIGN_UP(123, 4);
+    PRINT_ALIGN_UP(124, 4);
+
+    PRINT_ALIGN_UP(125, 4);
+    PRINT_ALIGN_UP(126, 4);
+    PRINT_ALIGN_UP(127, 4);
+    PRINT_ALIGN_UP(128, 4);
+
+    PRINT_ALIGN_UP(129, 4);
+    PRINT_ALIGN_UP(130, 4);
+    PRINT_ALIGN_UP(131, 4);
+    PRINT_ALIGN_UP(132, 4);
+
+    return 0;
+}
