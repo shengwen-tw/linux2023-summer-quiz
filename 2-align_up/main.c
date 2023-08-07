87decdb3328332008d4211232e0c978369fd4e23
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-08-07 20:10:49 +0800

    Rewrite problem 2 without using alignment variable

--- 2-align_up/main.c
+++ 2-align_up/main.c
@@ -9,7 +9,7 @@ static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
 {
     uintptr_t mask = alignment - 1;
     if ((alignment & mask) == 0) { /* power of two? */
-        return (sz & ~mask) + ((sz & mask) > 0) * alignment;
+        return (sz + mask) & ~mask;
     }
     return (((sz + mask) / alignment) * alignment);
 }
